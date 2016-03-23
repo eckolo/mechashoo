a6e7e53b31fea3aedc0c3525a51364177aa42012
eckolo <eckolo.0722@gmail.com>
2016-03-24 07:15:50 +0900

    試作弾丸発射

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,30 @@ public class Player : MonoBehaviour
     // 移動スピード
     public float speed;
 
+    // 弾を撃つ間隔
+    public float shotDelay;
+    private float delayCount;
+
+    // 弾のPrefab
+    public GameObject bullet;
+
+    // 弾を撃つかどうか
+    public bool canShot;
+
+    // 爆発のPrefab
+    public GameObject explosion;
+
+    // アニメーターコンポーネント
+    private Animator animator;
+
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Start()
+    {
+        delayCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         // 右・左
         float x = Input.GetAxisRaw("Horizontal");
@@ -20,12 +37,28 @@ public class Player : MonoBehaviour
         // 上・下
         float y = Input.GetAxisRaw("Vertical");
 
+        // 発射
+        var pushedZ = Input.GetKey("z");
+
         // 移動する向きを求める
         Vector2 direction = new Vector2(x, y).normalized;
 
         // 移動の制限
         Move(direction);
 
+        if (delayCount-- <= 0)
+        {
+            delayCount = shotDelay;
+
+            if (pushedZ)
+            {
+                // 弾をプレイヤーと同じ位置/角度で作成
+                Shot(transform);
+
+                // ショット音を鳴らす
+                GetComponent<AudioSource>().Play();
+            }
+        }
     }
 
     // 機体の移動
@@ -49,5 +82,11 @@ public class Player : MonoBehaviour
 
         // 制限をかけた値をプレイヤーの位置とする
         transform.position = pos;
+    }
+
+    // 弾の作成
+    public void Shot(Transform origin)
+    {
+        Instantiate(bullet, origin.position, origin.rotation);
     }
 }

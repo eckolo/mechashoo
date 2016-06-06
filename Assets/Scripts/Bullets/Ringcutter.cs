0ee797d7ccfa114b062b1e0adb5a76f846807fc2
eckolo <eckolo.0722@gmail.com>
2016-06-07 08:01:31 +0900

    新武装追加

---
+++ Assets/Scripts/Bullets/Ringcutter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ringcutter : Bullet
+{
+
+    // Use this for initialization
+    public override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+
+    }
+}

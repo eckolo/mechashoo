4629af1ab44cd9a572f4386c35fccfb79bb13bb8
eckolo <eckolo.0722@gmail.com>
2016-06-13 04:07:40 +0900

    弾丸系の挙動微修正

--- Assets/Scripts/Bullets/Shell.cs
+++ Assets/Scripts/Bullets/Shell.cs
@@ -5,7 +5,7 @@ public class Shell : Bullet
 //Bulletの中でも特に弾丸っぽいやつ
 {
     //弾丸が自動で移動方向を向く
-    public override void setVerosityAction(Vector2 verosity, float speed)
+    protected override void setVerosityAction(Vector2 verosity, float speed)
     {
         transform.rotation = Quaternion.FromToRotation(Vector2.right, verosity);
     }

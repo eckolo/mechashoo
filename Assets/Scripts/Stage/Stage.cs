cd6df004cd6e97f61ac2f1a07315beccf088e795
eckolo <eckolo.0722@gmail.com>
2016-06-22 08:39:46 +0900

    ステージ自動生成システム試作

---
+++ Assets/Scripts/Stage/Stage.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+///各ステージ動作の基底クラス
+/// </summary>
+public class Stage : MonoBehaviour
+{
+    /// <summary>
+    ///経過時間
+    /// </summary>
+    protected int elapsedFlame = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(stageAction());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedFlame += 1;
+    }
+
+    protected virtual IEnumerator stageAction()
+    {
+        yield break;
+    }
+
+    /// <summary>
+    ///オブジェクト配置関数
+    /// </summary>
+    protected Material setObject(Material setedObject, Vector2 coordinate)
+    {
+        Vector2 precisionCoordinate = Camera.main.ViewportToWorldPoint(coordinate + Vector2.right);
+
+        var newObject = (Material)Instantiate(setedObject, precisionCoordinate, transform.rotation);
+
+        return newObject;
+    }
+}
---
+++ Assets/Scripts/Stage/testStage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class testStage : Stage
+{
+    public List<Ship> enemyList = new List<Ship>();
+
+    protected override IEnumerator stageAction()
+    {
+        for (int i = 0; i < 12000; i++)
+        {
+            if (i % 1000 == 0) setObject(enemyList[0], new Vector2(0, 0.5f));
+            yield return null;
+        }
+    }
+}

74335658db86a306bcb8eeed89751b9a42dde734
eckolo <eckolo.0722@gmail.com>
2016-07-18 03:07:04 +0900

    Shell起点に噴煙的なものの仕組み実装

--- Assets/Scripts/Bullets/Shell.cs
+++ Assets/Scripts/Bullets/Shell.cs
@@ -19,6 +19,22 @@ public class Shell : Bullet
     protected List<int> accelerationTimeLimits = new List<int>();
 
     /// <summary>
+    ///通過後に発生する系のエフェクト
+    /// </summary>
+    [SerializeField]
+    protected Roots locus;
+    /// <summary>
+    ///通過後に発生する系のエフェクトの発生間隔
+    /// </summary>
+    [SerializeField]
+    protected int locusInterval = 0;
+    /// <summary>
+    ///通過後に発生する系のエフェクトの発生位置
+    /// </summary>
+    [SerializeField]
+    protected Vector2 locusPosition = new Vector2(0, 0);
+
+    /// <summary>
     ///弾丸が自動で移動方向を向く
     /// </summary>
     protected override void setVerosityAction(Vector2 speed)
@@ -41,12 +57,28 @@ public class Shell : Bullet
                 Vector2 setVector = nowSpeed.magnitude != 0
                     ? nowSpeed.normalized
                     : initialVelocity;
-                
+
                 setVerosity(setVector, setSpeed);
+
+                generateLocus(time);
 
                 yield return null;
             }
         }
         yield break;
+    }
+    private void generateLocus(int time)
+    {
+        var setedLocus = locus;
+        if (setedLocus == null) return;
+        if (time % Mathf.Max(locusInterval + 1, 1) > 0) return;
+
+        Vector2 locusPositionLocal = transform.rotation * locusPosition;
+        locusPositionLocal = (Vector2)transform.position
+            + new Vector2(locusPositionLocal.x * getLossyScale().x,
+            locusPositionLocal.y * getLossyScale().y);
+
+
+        Instantiate(locus, locusPositionLocal, transform.rotation);
     }
 }

7dba2feb0f269ab6653b4283a9412eb2a6123ccd
eckolo <eckolo.0722@gmail.com>
2016-08-20 20:33:39 +0900

    爆発エフェクトを描き直し

--- Assets/Scripts/Effects/Explosion.cs
+++ Assets/Scripts/Effects/Explosion.cs
@@ -31,7 +31,7 @@ public class Explosion : Effect
                 ? easing.exponential.Out(maxSize, time, destroyLimit - 1)
                 : easing.exponential.SubOut(maxSize, time, destroyLimit - 1));
 
-            setAlpha(nowAlpha * (easing.quadratic.SubOut(time, destroyLimit - 1)));
+            setAlpha(nowAlpha * (easing.quadratic.SubIn(time, destroyLimit - 1)));
 
             yield return null;
         }

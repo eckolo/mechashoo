30177fc9705ccd86121e6d969133866b75a0b798
eckolo <eckolo.0722@gmail.com>
2016-09-11 18:22:46 +0900

    メニュー周りの階層構造の基礎組み完成

--- Assets/Scripts/Stage/Menu.cs
+++ Assets/Scripts/Stage/Menu.cs
@@ -6,24 +6,28 @@ using System;
 public class Menu : Stage
 {
     static Vector2 menuPosition = new Vector2(-240, 180);
+    static IEnumerator nextAction = null;
+    delegate IEnumerator Action();
 
     class MenuState
     {
         public string text;
         public bool ableChoice;
+        public Action action;
     }
-    static MenuState getMenu(string text, bool ableChoice)
+    static MenuState getMenu(string text, Action action, bool ableChoice)
     {
         return new MenuState
         {
             text = text,
+            action = action,
             ableChoice = ableChoice
         };
     }
-    List<MenuState> mainMenus = new List<MenuState>
+    static List<MenuState> mainMenus = new List<MenuState>
     {
-        getMenu("戦場選択",true),
-        getMenu("機体選択",true)
+        getMenu("戦場選択",goNextStage,true),
+        getMenu("機体選択",selectShip,true)
     };
 
     public override void startStageAction()
@@ -34,51 +38,65 @@ public class Menu : Stage
     protected override IEnumerator stageAction()
     {
         yield return mainMuneAction();
+        while (nextAction != null)
+        {
+            var runAction = nextAction;
+            nextAction = mainMuneAction();
+            yield return runAction;
+        }
 
         stopStageAction();
         yield break;
     }
 
-    protected IEnumerator mainMuneAction()
+    static IEnumerator mainMuneAction()
     {
         List<string> menus = new List<string>();
         for (int i = 0; i < mainMenus.Count; i++)
             menus.Add(mainMenus[i].ableChoice ? mainMenus[i].text : "");
         yield return getChoices(menus, setPosition: menuPosition);
 
-        var selectedAction = lastSelected % mainMenus.Count;
-        IEnumerator nextAction = stageAction();
-        switch (selectedAction)
-        {
-            case 0:
-                nextAction = goNextStage();
-                break;
-            default:
-                break;
-        }
+        nextAction = mainMenus[lastSelected ?? 0 % mainMenus.Count].action();
 
-        yield return nextAction;
         yield break;
     }
 
-    protected IEnumerator goNextStage()
+    static IEnumerator goNextStage()
     {
         List<string> stageNames = new List<string>();
-        var stages = mainSystem.stages;
+        var stages = Sys.stages;
         for (int i = 0; i < stages.Count; i++)
             stageNames.Add(stages[i].ableChoice && !stages[i].isSystem ? stages[i].stageName : "");
-        yield return getChoices(stageNames, setPosition: menuPosition);
+
+        yield return getChoices(stageNames,
+            setPosition: menuPosition,
+            ableCancel: true);
 
         if (lastSelected >= 0)
         {
-            mainSystem.nextStageNum = lastSelected ?? 0;
-            stopStageAction();
-        }
-        else
-        {
-            yield return mainMuneAction();
+            Sys.nextStageNum = lastSelected ?? 0;
+            nextAction = null;
         }
 
+        yield break;
+    }
+
+    static IEnumerator selectShip()
+    {
+        setPlayer();
+
+        var keepSipData = sysPlayer.coreData;
+
+        List<string> ships = new List<string>();
+        for (var i = 0; i < Sys.selectShip.Count; i++) ships.Add(Sys.selectShip[i].gameObject.name);
+
+        yield return getChoices(ships,
+            action: i => sysPlayer.setCoreStatus(Sys.selectShip[i]),
+            setPosition: Vector2.down * 90,
+            ableCancel: true);
+        if (lastSelected < 0) sysPlayer.setCoreStatus(keepSipData);
+
+        //transparentPlayer();
         yield break;
     }
 }

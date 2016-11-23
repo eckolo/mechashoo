60e56682ef2ae3f6335459031cac77ed9a8afa27
eckolo <eckolo.0722@gmail.com>
2016-11-24 06:38:59 +0900

    ユニットテストのテスト

--- Assets/Scripts/Editor/TEST.cs
+++ Assets/Scripts/Editor/TEST.cs
@@ -4,11 +4,23 @@ using NUnit.Framework;
 
 public static class TEST
 {
-    public static class Methods
+    [Test]
+    public static void UnitTestTest1()
     {
+        //Arrange
+        var gameObject = new GameObject();
+
+        //Act
+        //Try to rename the GameObject
+        var newGameObjectName = "My game object";
+        gameObject.name = newGameObjectName;
+
+        //Assert
+        //The object has a new name
+        Assert.AreEqual(newGameObjectName, gameObject.name);
     }
     [Test]
-    public static void Start()
+    public static void UnitTestTest2()
     {
         //Arrange
         var gameObject = new GameObject();

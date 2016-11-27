38f47426752d3626ac0146210b6c3aa146b18b0f
eckolo <eckolo.0722@gmail.com>
2016-11-27 12:38:01 +0900

    ユニットテストのテスト

--- Assets/Scripts/Editor/TEST.cs
+++ Assets/Scripts/Editor/TEST.cs
@@ -2,36 +2,35 @@
 using UnityEditor;
 using NUnit.Framework;
 
-public static class TEST
-{
+public static partial class TEST {
     [Test]
-    public static void UnitTestTest1()
-    {
+    public static void UnitTestTest1() {
         //Arrange
         var gameObject = new GameObject();
 
         //Act
         //Try to rename the GameObject
-        var newGameObjectName = "My game object";
+        var newGameObjectName = "UnitTestTest1";
         gameObject.name = newGameObjectName;
 
         //Assert
         //The object has a new name
         Assert.AreEqual(newGameObjectName, gameObject.name);
     }
-    [Test]
-    public static void UnitTestTest2()
-    {
-        //Arrange
-        var gameObject = new GameObject();
+    public static class InnerTEST {
+        [Test]
+        public static void InnerUnitTestTest1() {
+            //Arrange
+            var gameObject = new GameObject();
 
-        //Act
-        //Try to rename the GameObject
-        var newGameObjectName = "My game object";
-        gameObject.name = newGameObjectName;
+            //Act
+            //Try to rename the GameObject
+            var newGameObjectName = "InnerUnitTestTest1";
+            gameObject.name = newGameObjectName;
 
-        //Assert
-        //The object has a new name
-        Assert.AreEqual(newGameObjectName, gameObject.name);
+            //Assert
+            //The object has a new name
+            Assert.AreEqual(newGameObjectName, gameObject.name);
+        }
     }
 }

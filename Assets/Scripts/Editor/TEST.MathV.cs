8f68e7a98e0a2c1072761c5a70307cc26d099ee6
eckolo <eckolo.0722@gmail.com>
2016-11-27 14:52:46 +0900

    やはりMonoBehaviourは直でnewすると怒られるっぽい

--- Assets/Scripts/Editor/TEST.MathV.cs
+++ Assets/Scripts/Editor/TEST.MathV.cs
@@ -9,10 +9,11 @@ public static partial class TEST {
     public class MethodMathV : Methods {
         [Test]
         public static void max() {
+            var method = new GameObject().AddComponent<Methods>();
             var maxValue = (Vector2)
-                new Methods().GetType()
+                method.GetType()
                 .GetMethod("MathV.max", BindingFlags.NonPublic | BindingFlags.Instance)
-                .Invoke(new Methods(), new object[] { new Vector2(3, 4), new Vector2(5, 5) });
+                .Invoke(method, new object[] { new Vector2(3, 4), new Vector2(5, 5) });
 
             Assert.AreEqual(new Vector2(5, 5), maxValue);
         }

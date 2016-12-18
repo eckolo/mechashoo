2294fe1f0ba734776aca578ceb05a1e129b843d4
eckolo <eckolo.0722@gmail.com>
2016-12-19 00:45:40 +0900

    テストステージと演習場を分ける

---
+++ Assets/Scripts/Stage/ExerciseStage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExerciseStage : Stage
+{
+    protected override IEnumerator stageAction()
+    {
+        yield return wait(1);
+
+        yield return stageAction();
+    }
+}

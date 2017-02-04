5827adbf5069ef3640b9ebe9c827a8bdae67f633
eckolo <eckolo.0722@gmail.com>
2017-02-04 23:20:54 +0900

    csprojを追跡に含んだついでにJodimucuji追加

---
+++ Assets/Scripts/Ships/Jodimucuji.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jodimucuji : Npc
+{
+    protected override IEnumerator motion(int actionNum)
+    {
+        int interval = 100 - (int)(shipLevel / 10);
+        var nearTarget = nowNearTarget;
+
+        switch(nowActionState)
+        {
+            case ActionPattern.NON_COMBAT:
+                nextActionState = ActionPattern.NON_COMBAT;
+                exertPower(nowForward, reactPower, (lowerSpeed + maximumSpeed) / 2);
+                break;
+            case ActionPattern.MOVE:
+                nextActionState = ActionPattern.AIMING;
+                var destination = position + siteAlignment;
+                for(var time = 0; time < interval * 2; time++)
+                {
+                    exertPower(destination - position, reactPower, maximumSpeed);
+                    aiming(nearTarget.position);
+
+                    destination = MathV.correct(destination, nearTarget.position, 0.999f);
+                    yield return wait(1);
+                }
+                break;
+            case ActionPattern.AIMING:
+                nextActionState = ActionPattern.ATTACK;
+                yield return aimingAction(nearTarget.position, () => stopping());
+                break;
+            case ActionPattern.ATTACK:
+                nextActionState = ActionPattern.MOVE;
+
+                int armNum = toInt(siteAlignment.magnitude < arms[1].tipLength);
+                arms[armNum].tipHand.actionWeapon(Weapon.ActionType.NOMAL);
+
+                yield return wait(interval);
+                break;
+            default:
+                break;
+        }
+
+        yield break;
+    }
+}

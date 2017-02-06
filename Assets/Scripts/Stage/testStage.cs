2f64802aaaddbf0557d5a34bfab2d2980b9bbe6b
eckolo <eckolo.0722@gmail.com>
2017-02-07 06:09:03 +0900

    テストステージをそれっぽい感じに調整

--- Assets/Scripts/Stage/testStage.cs
+++ Assets/Scripts/Stage/testStage.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class TestStage : Stage
 {
     protected override IEnumerator stageAction()
     {
-        int enemyCount = enemyList.Count;
-        setEnemy(Random.Range(0, enemyCount), new Vector2(0, Random.value));
-        for(int index = 0; index < 2; index++)
+        var limit = 6;
+        var enemyCount = enemyList.Count - 1;
+        for(int index = 0; index < limit; index++)
         {
-            yield return wait(1000 - (int)stageLevel++);
-            setEnemy(Random.Range(0, enemyCount), new Vector2(0, Random.value));
+            yield return wait(720);
+            setEnemy(Random.Range(0, enemyCount), new Vector2(0, (float)index / limit));
         }
+        while(allEnemies.Any()) yield return wait(1);
+        var boss = setEnemy(enemyCount, new Vector2(0, 0.5f));
     }
 }

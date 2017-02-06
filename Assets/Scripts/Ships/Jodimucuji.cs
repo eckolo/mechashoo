eba9817521c334866e076e66ada7469ed349077c
eckolo <eckolo.0722@gmail.com>
2017-02-07 05:48:16 +0900

    Jodimucujiのモーション作成

--- Assets/Scripts/Ships/Jodimucuji.cs
+++ Assets/Scripts/Ships/Jodimucuji.cs
@@ -1,42 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using static Npc.ActionPattern;
 
 public class Jodimucuji : Npc
 {
+    //0:grenade
+    //1:assaulter
+    //2:laser
     protected override IEnumerator motion(int actionNum)
     {
-        int interval = 100 - (int)(shipLevel / 10);
+        int interval = Mathf.FloorToInt(Mathf.Max(100 - shipLevel, 1));
+        var moderateSpeed = (lowerSpeed + maximumSpeed) / 2;
         var nearTarget = nowNearTarget;
 
         switch(nowActionState)
         {
-            case ActionPattern.NON_COMBAT:
-                nextActionState = ActionPattern.NON_COMBAT;
-                exertPower(nowForward, reactPower, (lowerSpeed + maximumSpeed) / 2);
+            case NON_COMBAT:
+                nextActionState = NON_COMBAT;
+                exertPower(nowForward, reactPower, moderateSpeed);
                 break;
-            case ActionPattern.MOVE:
-                nextActionState = ActionPattern.AIMING;
+            case MOVE:
+                nextActionState = AIMING;
                 var destination = position + siteAlignment;
-                for(var time = 0; time < interval * 2; time++)
-                {
-                    exertPower(destination - position, reactPower, maximumSpeed);
-                    aiming(nearTarget.position);
-
+                yield return aimingAction(() => nearTarget.position, interval * 2, () => {
+                    exertPower(destination - position, reactPower, moderateSpeed);
                     destination = MathV.correct(destination, nearTarget.position, 0.999f);
-                    yield return wait(1);
+                });
+                nextActionIndex = Random.Range(0, allWeapons.Count);
+                break;
+            case AIMING:
+                nextActionState = ATTACK;
+                switch(actionNum)
+                {
+                    case 0:
+                        yield return aimingAction(nearTarget.position, () => nowSpeed.magnitude > 0, () => stopping());
+                        break;
+                    case 1:
+                        yield return aimingAction(() => nearTarget.position + nearTarget.nowSpeed, () => nowSpeed.magnitude > 0, () => stopping());
+                        break;
+                    case 2:
+                        var _destination = new Vector2(position.x, (nearTarget.position + nearTarget.nowSpeed).y);
+                        yield return headingDestination(_destination, maximumSpeed);
+                        break;
+                    default:
+                        break;
                 }
                 break;
-            case ActionPattern.AIMING:
-                nextActionState = ActionPattern.ATTACK;
-                yield return aimingAction(nearTarget.position, () => stopping());
-                break;
-            case ActionPattern.ATTACK:
-                nextActionState = ActionPattern.MOVE;
-
-                int armNum = toInt(siteAlignment.magnitude < arms[1].tipLength);
-                arms[armNum].tipHand.actionWeapon(Weapon.ActionType.NOMAL);
-
-                yield return wait(interval);
+            case ATTACK:
+                allWeapons[actionNum].action(Weapon.ActionType.NOMAL);
+                var continuous = actionNum == 1
+                    && selectRandom(new List<int> { 2, 1 }, new List<bool> { true, false });
+                if(continuous)
+                {
+                    for(int index = 0; index < 1 + shipLevel; index++)
+                    {
+                        var weapon = allWeapons[actionNum];
+                        yield return aimingAction(() => nearTarget.position + nearTarget.nowSpeed,
+                            interval / 2,
+                            () => exertPower(nearTarget.position + nearTarget.nowSpeed - position - siteAlignment, reactPower, lowerSpeed));
+                        yield return aimingAction(nearTarget.position + nearTarget.nowSpeed,
+                            () => !weapon.canAction,
+                            () => exertPower(nearTarget.position + nearTarget.nowSpeed - position - siteAlignment, reactPower, lowerSpeed));
+                        weapon.action(Weapon.ActionType.NOMAL);
+                    }
+                }
+                if(actionNum == 2)
+                {
+                    nextActionState = selectRandom(new List<int> { 1, 2 }, new List<ActionPattern> { AIMING, MOVE });
+                    nextActionIndex = Random.Range(0, allWeapons.Count(weapon => weapon != allWeapons[actionNum]));
+                    yield return stopping();
+                    yield return aimingAction(() => nearTarget.position, interval);
+                }
+                else
+                {
+                    nextActionState = MOVE;
+                    yield return wait(interval);
+                }
                 break;
             default:
                 break;

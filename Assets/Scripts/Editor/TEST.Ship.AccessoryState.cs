e2e85ecafa7def41d3951a338a28ecda47341e3b
eckolo <eckolo.0722@gmail.com>
2017-02-19 13:55:54 +0900

    その他Shipの接続部系のテスト追加

---
+++ Assets/Scripts/Editor/TEST.Ship.AccessoryState.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public static partial class TEST
+{
+    public static partial class ShipTEST
+    {
+        public static class AccessoryStateTEST
+        {
+            [Test]
+            public static void Equals()
+            {
+                var accessoryState1 = new Ship.AccessoryState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8
+                };
+                var accessoryState2 = new Ship.AccessoryState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8,
+                    entity = new GameObject("test1", typeof(Accessory)).GetComponent<Accessory>()
+                };
+                var accessoryState3 = new Ship.AccessoryState
+                {
+                    rootPosition = new Vector2(24, 56.7f),
+                    positionZ = 672,
+                    partsNum = 138,
+                    entity = new GameObject("test2", typeof(Accessory)).GetComponent<Accessory>()
+                };
+                var accessoryState4 = new Ship.AccessoryState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8
+                };
+
+                Assert.IsTrue(accessoryState1.Equals(accessoryState1));
+                Assert.IsFalse(accessoryState1.Equals(accessoryState2));
+                Assert.IsFalse(accessoryState1.Equals(accessoryState3));
+                Assert.IsTrue(accessoryState1.Equals(accessoryState4));
+
+                Assert.IsFalse(accessoryState2.Equals(accessoryState1));
+                Assert.IsTrue(accessoryState2.Equals(accessoryState2));
+                Assert.IsFalse(accessoryState2.Equals(accessoryState3));
+                Assert.IsFalse(accessoryState2.Equals(accessoryState4));
+
+                Assert.IsFalse(accessoryState3.Equals(accessoryState1));
+                Assert.IsFalse(accessoryState3.Equals(accessoryState2));
+                Assert.IsTrue(accessoryState3.Equals(accessoryState3));
+                Assert.IsFalse(accessoryState3.Equals(accessoryState4));
+
+                Assert.IsTrue(accessoryState4.Equals(accessoryState1));
+                Assert.IsFalse(accessoryState4.Equals(accessoryState2));
+                Assert.IsFalse(accessoryState4.Equals(accessoryState3));
+                Assert.IsTrue(accessoryState4.Equals(accessoryState4));
+            }
+            [Test]
+            public static void myself()
+            {
+                var accessoryState1 = new Ship.AccessoryState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8,
+                    entity = new GameObject("test1", typeof(Accessory)).GetComponent<Accessory>()
+                };
+                var accessoryState2 = accessoryState1.myself;
+
+                Assert.IsTrue(accessoryState1.Equals(accessoryState2));
+                Assert.IsFalse(accessoryState1 == accessoryState2);
+
+                accessoryState1.positionZ = 120.544f;
+
+                Assert.IsFalse(accessoryState1.Equals(accessoryState2));
+                Assert.IsFalse(accessoryState1 == accessoryState2);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/TEST.Ship.ArmState.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public static partial class TEST
+{
+    public static partial class ShipTEST
+    {
+        public static class ArmStateTEST
+        {
+            [Test]
+            public static void Equals()
+            {
+                var armState1 = new Ship.ArmState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8,
+                    alignment = new Vector2(548, 569f)
+                };
+                var armState2 = new Ship.ArmState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8,
+                    entity = new GameObject("test1", typeof(Arm)).GetComponent<Arm>(),
+                    alignment = new Vector2(548, 569f)
+                };
+                var armState3 = new Ship.ArmState
+                {
+                    rootPosition = new Vector2(24, 56.7f),
+                    positionZ = 672,
+                    partsNum = 138,
+                    entity = new GameObject("test2", typeof(Arm)).GetComponent<Arm>(),
+                    alignment = new Vector2(54.58f, 5693)
+                };
+                var armState4 = new Ship.ArmState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8,
+                    alignment = new Vector2(548, 569f)
+                };
+
+                Assert.IsTrue(armState1.Equals(armState1));
+                Assert.IsFalse(armState1.Equals(armState2));
+                Assert.IsFalse(armState1.Equals(armState3));
+                Assert.IsTrue(armState1.Equals(armState4));
+
+                Assert.IsFalse(armState2.Equals(armState1));
+                Assert.IsTrue(armState2.Equals(armState2));
+                Assert.IsFalse(armState2.Equals(armState3));
+                Assert.IsFalse(armState2.Equals(armState4));
+
+                Assert.IsFalse(armState3.Equals(armState1));
+                Assert.IsFalse(armState3.Equals(armState2));
+                Assert.IsTrue(armState3.Equals(armState3));
+                Assert.IsFalse(armState3.Equals(armState4));
+
+                Assert.IsTrue(armState4.Equals(armState1));
+                Assert.IsFalse(armState4.Equals(armState2));
+                Assert.IsFalse(armState4.Equals(armState3));
+                Assert.IsTrue(armState4.Equals(armState4));
+            }
+            [Test]
+            public static void myself()
+            {
+                var armState1 = new Ship.ArmState
+                {
+                    rootPosition = new Vector2(2.4f, 567),
+                    positionZ = 5.672f,
+                    partsNum = 8,
+                    entity = new GameObject("test1", typeof(Arm)).GetComponent<Arm>(),
+                    alignment = new Vector2(548, 569f)
+                };
+                var armState2 = armState1.myself;
+
+                Assert.IsTrue(armState1.Equals(armState2));
+                Assert.IsFalse(armState1 == armState2);
+
+                armState1.positionZ = 120.544f;
+
+                Assert.IsFalse(armState1.Equals(armState2));
+                Assert.IsFalse(armState1 == armState2);
+            }
+        }
+    }
+}

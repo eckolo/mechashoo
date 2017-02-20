41d3bb70d3b883402e1a17473fb6b2486122cc45
eckolo <eckolo.0722@gmail.com>
2017-02-21 05:19:55 +0900

    Luwucijiqiのスクリプト追加

---
+++ Assets/Scripts/Ships/Luwucijiqi.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Luwucijiqi : Npc
+{
+    protected override IEnumerator motion(int actionNum)
+    {
+        int interval = Mathf.FloorToInt(Mathf.Max(100 - shipLevel, 1));
+        var nearTarget = nowNearTarget;
+
+        switch(nowActionState)
+        {
+            case ActionPattern.MOVE:
+                nextActionState = ActionPattern.AIMING;
+                var destination = position + siteAlignment;
+                yield return aimingAction(nearTarget.position, interval * 2, () => {
+                    exertPower(destination - position, reactPower, maximumSpeed);
+                    destination = destination.correct(nearTarget.position, 0.999f);
+                });
+                break;
+            case ActionPattern.AIMING:
+                nextActionState = ActionPattern.ATTACK;
+                nextActionIndex = (nearTarget.position.magnitude < arms[1].tipLength).toInt();
+                yield return aimingAction(() => nearTarget.position, () => nowSpeed.magnitude > 0, () => stopping());
+                break;
+            case ActionPattern.ATTACK:
+                nextActionState = ActionPattern.MOVE;
+
+                int armNum = (siteAlignment.magnitude < arms[1].tipLength).toInt();
+                arms[armNum].tipHand.actionWeapon(Weapon.ActionType.NOMAL);
+
+                yield return wait(interval);
+                break;
+            default:
+                break;
+        }
+
+        yield break;
+    }
+}

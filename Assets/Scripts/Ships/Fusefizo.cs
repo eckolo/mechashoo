8d3a4c991cf67a12a6676945c295e43e29c53ad0
eckolo <eckolo.0722@gmail.com>
2017-03-02 07:54:54 +0900

    Fusefizoのモーション作成

--- Assets/Scripts/Ships/Fusefizo.cs
+++ Assets/Scripts/Ships/Fusefizo.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 /// <summary>
 /// 小型蟹機クラス
@@ -9,30 +10,50 @@ public class Fusefizo : Npc
     protected override IEnumerator motion(int actionNum)
     {
         int interval = Mathf.FloorToInt(Mathf.Max(100 - shipLevel, 1));
+        var destination = new[] { -90, 0, 90 }.selectRandom(new[] { 3, 2, 3 });
         var nearTarget = nowNearTarget;
 
         switch(nowActionState)
         {
             case ActionPattern.MOVE:
                 nextActionState = ActionPattern.AIMING;
-                var direction = nowForward;
-                yield return aimingAction(nearTarget.position, interval * 2, () => exertPower(direction, reactPower, maximumSpeed));
+                var targetPosition = nearTarget.position - position;
+                yield return aimingAction(() => nearTarget.position,
+                    interval * 2,
+                    () => exertPower(getProperPosition(targetPosition, destination), reactPower, maximumSpeed));
+                nextActionIndex = new[] { 0, 1 }.selectRandom(new[] { 2, 3 });
                 break;
             case ActionPattern.AIMING:
                 nextActionState = ActionPattern.ATTACK;
-                yield return aimingAction(nearTarget.position, () => exertPower(nowForward, reactPower, lowerSpeed), 1);
+                var alreadyAttack = false;
+                if(actionNum != 0) yield return aimingAction(() => nearTarget.position,
+                        interval,
+                       () => {
+                           exertPower(getProperPosition(nearTarget, destination), reactPower, (lowerSpeed + maximumSpeed) / 2);
+                           if((nearTarget.position - position).magnitude < properDistance)
+                           {
+                               if(!alreadyAttack)
+                               {
+                                   PinchAttack();
+                                   alreadyAttack = true;
+                               }
+                           }
+                           else if(alreadyAttack) alreadyAttack = false;
+                       });
                 break;
             case ActionPattern.ATTACK:
                 nextActionState = ActionPattern.MOVE;
-                foreach(var weapon in bodyWeapons)
+                yield return stoppingAction(lowerSpeed);
+                if(actionNum != 0)
                 {
-                    if(weapon == null) continue;
-                    weapon.action();
+                    SwingAttack();
+                    yield return aimingAction(() => nearTarget.position, interval);
+                    yield return wait(() => !allWeapons.Any(weapon => !weapon.canAction));
                 }
-                for(var time = 0; time < interval; time++)
+                else
                 {
-                    stopping();
-                    yield return wait(1);
+                    PinchAttack();
+                    yield return wait(interval, () => !allWeapons.Any(weapon => !weapon.canAction));
                 }
                 break;
             default:
@@ -40,5 +61,27 @@ public class Fusefizo : Npc
         }
 
         yield break;
+    }
+    void PinchAttack()
+    {
+        foreach(var handWeapon in handWeapons)
+        {
+            if(handWeapon.canAction)
+            {
+                handWeapon.action(Weapon.ActionType.NOMAL);
+                break;
+            }
+        }
+    }
+    void SwingAttack()
+    {
+        foreach(var handWeapon in handWeapons)
+        {
+            if(handWeapon.canAction)
+            {
+                handWeapon.action(Weapon.ActionType.SINK);
+                break;
+            }
+        }
     }
 }

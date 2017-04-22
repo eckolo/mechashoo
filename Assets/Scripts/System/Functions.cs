5814a2cf2bd9d0464b161d276cb4f4321fb8cc54
eckolo <eckolo.0722@gmail.com>
2017-04-22 22:20:59 +0900

    Textオブジェクトを操作する拡張関数群をFunctionから切り出し

--- Assets/Scripts/System/Functions.cs
+++ Assets/Scripts/System/Functions.cs
@@ -119,29 +119,6 @@ public static class Functions
     }
 
     /// <summary>
-    /// Textの透明度設定
-    /// </summary>
-    /// <param name="text">透明度設定先のオブジェクト</param>
-    /// <param name="alpha">透明度</param>
-    /// <returns></returns>
-    public static float setAlpha(this Text text, float alpha)
-    {
-        text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
-        return text.color.a;
-    }
-    /// <summary>
-    /// Textの位置設定
-    /// </summary>
-    /// <param name="text">位置設定先のオブジェクト</param>
-    /// <param name="position">位置</param>
-    /// <returns></returns>
-    public static Vector2 setPosition(this Text text, Vector2 position)
-    {
-        text.rectTransform.localPosition = new Vector3(position.x, position.y, text.rectTransform.localPosition.z);
-        return new Vector2(text.rectTransform.localPosition.x, text.rectTransform.localPosition.y);
-    }
-
-    /// <summary>
     /// 帯状のエフェクトを指定座標中心に配置する
     /// </summary>
     /// <param name="effect">表示するエフェクト</param>
@@ -177,16 +154,6 @@ public static class Functions
             }
         }
         return effectList;
-    }
-    /// <summary>
-    ///システムテキストの削除
-    /// </summary>
-    public static string selfDestroy(this Text text, bool system = false)
-    {
-        if(text == null) return "";
-        var result = Methods.getSysText(text);
-        Object.Destroy(text.gameObject);
-        return result;
     }
     /// <summary>
     /// リストの値コピー
---
+++ Assets/Scripts/System/TextFunctions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+/// <summary>
+/// Textオブジェクトを操作する拡張関数群
+/// </summary>
+public static class TextFunctions
+{
+    /// <summary>
+    /// Textの透明度設定
+    /// </summary>
+    /// <param name="text">透明度設定先のオブジェクト</param>
+    /// <param name="alpha">透明度</param>
+    /// <returns></returns>
+    public static float setAlpha(this Text text, float alpha)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+        return text.color.a;
+    }
+    /// <summary>
+    /// Textの位置設定
+    /// </summary>
+    /// <param name="text">位置設定先のオブジェクト</param>
+    /// <param name="position">位置</param>
+    /// <returns></returns>
+    public static Vector2 setPosition(this Text text, Vector2 position)
+    {
+        text.rectTransform.localPosition = new Vector3(position.x, position.y, text.rectTransform.localPosition.z);
+        return new Vector2(text.rectTransform.localPosition.x, text.rectTransform.localPosition.y);
+    }
+    /// <summary>
+    ///システムテキストの削除
+    /// </summary>
+    public static string selfDestroy(this Text text, bool system = false)
+    {
+        if(text == null) return "";
+        var result = Methods.getSysText(text);
+        Object.Destroy(text.gameObject);
+        return result;
+    }
+}

e65d038c9d13a3cd1af04badf638cfeedf77ce81
eckolo <eckolo.0722@gmail.com>
2017-06-12 05:42:01 +0900

    Weapon周りのリファクタリング

---
+++ Assets/Scripts/Weapons/Weapon.Injection.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public partial class Weapon : Parts
+{
+    /// <summary>
+    ///射出孔関連のパラメータ
+    /// </summary>
+    [Serializable]
+    public class Injection
+    {
+        /// <summary>
+        ///射出孔の座標
+        /// </summary>
+        public Vector2 hole = Vector2.zero;
+        /// <summary>
+        ///射出角補正
+        /// </summary>
+        public float angle = 0;
+        /// <summary>
+        ///初速度
+        /// </summary>
+        public float initialVelocity = 1;
+        /// <summary>
+        /// 射出時燃料消費量補正値
+        /// </summary>
+        public float fuelCostPar = 1;
+        /// <summary>
+        /// 連射数特殊指定
+        /// </summary>
+        public int burst = 0;
+        /// <summary>
+        ///射出弾丸の特殊指定
+        /// </summary>
+        public Bullet bullet = null;
+        /// <summary>
+        /// 射出後の弾丸を武装と連結状態にするフラグ
+        /// </summary>
+        public bool union = false;
+        /// <summary>
+        /// 弾ブレ度合い補正
+        /// </summary>
+        public float noAccuracy = 0;
+        /// <summary>
+        ///射出時の効果音
+        /// </summary>
+        public AudioClip se = null;
+        /// <summary>
+        /// 射出前のチャージエフェクト特殊指定
+        /// </summary>
+        public Effect charge = null;
+        /// <summary>
+        ///射出タイミング特殊指定
+        /// </summary>
+        public List<ActionType> timing = new List<ActionType>();
+    }
+}
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -8,67 +8,16 @@ using UnityEngine.Events;
 /// <summary>
 /// 武装クラス
 /// </summary>
-public class Weapon : Parts
+public partial class Weapon : Parts
 {
     /// <summary>
-    ///持ち手の座標
+    /// 持ち手の座標
     /// </summary>
     public Vector2 handlePosition = Vector2.zero;
     /// <summary>
-    ///持ち手の奥行き座標
+    /// 持ち手の奥行き座標
     /// </summary>
     public float handledZ = 1;
-    /// <summary>
-    ///射出孔関連のパラメータ
-    /// </summary>
-    [Serializable]
-    public class Injection
-    {
-        /// <summary>
-        ///射出孔の座標
-        /// </summary>
-        public Vector2 hole = Vector2.zero;
-        /// <summary>
-        ///射出角補正
-        /// </summary>
-        public float angle = 0;
-        /// <summary>
-        ///初速度
-        /// </summary>
-        public float initialVelocity = 1;
-        /// <summary>
-        /// 射出時燃料消費量補正値
-        /// </summary>
-        public float fuelCostPar = 1;
-        /// <summary>
-        /// 連射数特殊指定
-        /// </summary>
-        public int burst = 0;
-        /// <summary>
-        ///射出弾丸の特殊指定
-        /// </summary>
-        public Bullet bullet = null;
-        /// <summary>
-        /// 射出後の弾丸を武装と連結状態にするフラグ
-        /// </summary>
-        public bool union = false;
-        /// <summary>
-        /// 弾ブレ度合い補正
-        /// </summary>
-        public float noAccuracy = 0;
-        /// <summary>
-        ///射出時の効果音
-        /// </summary>
-        public AudioClip se = null;
-        /// <summary>
-        /// 射出前のチャージエフェクト特殊指定
-        /// </summary>
-        public Effect charge = null;
-        /// <summary>
-        ///射出タイミング特殊指定
-        /// </summary>
-        public List<ActionType> timing = new List<ActionType>();
-    }
     [SerializeField]
     private List<Injection> injections = new List<Injection>();
     public virtual List<Injection> nowInjections => injections;

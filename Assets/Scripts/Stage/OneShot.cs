86e5b7a64dcaa319efff857db8fc35605fe4f570
eckolo <eckolo.0722@gmail.com>
2017-07-18 22:55:29 +0900

    テストプレイ用ステージ実装

---
+++ Assets/Scripts/Stage/OneShot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Linq;
+
+public class OneShot : Stage
+{
+    protected override IEnumerator StageAction()
+    {
+        SetEnemy(0, 1.2f, 0);
+
+        yield return Wait(() => !allEnemies.Any());
+    }
+
+    protected override bool isComplete => !allEnemyObjects.Any();
+    public override bool challengeable => true;
+}

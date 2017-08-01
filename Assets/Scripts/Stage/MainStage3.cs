f1975abbb339b1f55d15c4b06d8f35276013b9d7
eckolo <eckolo.0722@gmail.com>
2017-08-01 13:10:09 +0900

    ステージ3をガワだけ実装

---
+++ Assets/Scripts/Stage/MainStage3.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class MainStage3 : Stage
+{
+    const int INTERVAL = 2400;
+    const int INTERVAL_A_LITTLE = INTERVAL / 10;
+
+    public override bool challengeable
+    {
+        get {
+            return sys.storyPhase >= 3;
+        }
+    }
+
+    protected override IEnumerator OpeningAction()
+    {
+        yield break;
+    }
+
+    protected override IEnumerator StageAction()
+    {
+        yield break;
+    }
+
+    protected override IEnumerator SuccessAction()
+    {
+        yield break;
+    }
+}
